192b59deea0314f0c4df25fba1a76cb121c6099b
cheerppy <john0217smith@gmail.com>
2023-05-15 01:21:31 +0900

    remove lastmod

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
 export default {
-    created() {
-        const lastmodDate = new Date(document.lastModified);
-        const timezoneOffset = new Date().getTimezoneOffset()
-        const lastmodStr = lastmodDate.getFullYear() + "/" + (lastmodDate.getMonth() + 1) + "/" + lastmodDate.getDate() + " " + lastmodDate.getHours() + ":" + ("0" + lastmodDate.getMinutes()).slice(-2) + ":" + ("0" + lastmodDate.getSeconds()).slice(-2)
-        const timezone = (timezoneOffset === -540) ? "(JST)" : `GMT ${(timezoneOffset >= 0) ? "+" : ""}${Math.floor(timezoneOffset / -60)}:${timezoneOffset / 60}`
-        this.lastmodStr = lastmodStr;
-        this.timezone = timezone;
-    },
+    // created() {
+    //     const lastmodDate = new Date(document.lastModified);
+    //     const timezoneOffset = new Date().getTimezoneOffset()
+    //     const lastmodStr = lastmodDate.getFullYear() + "/" + (lastmodDate.getMonth() + 1) + "/" + lastmodDate.getDate() + " " + lastmodDate.getHours() + ":" + ("0" + lastmodDate.getMinutes()).slice(-2) + ":" + ("0" + lastmodDate.getSeconds()).slice(-2)
+    //     const timezone = (timezoneOffset === -540) ? "(JST)" : `GMT ${(timezoneOffset >= 0) ? "+" : ""}${Math.floor(timezoneOffset / -60)}:${timezoneOffset / 60}`
+    //     this.lastmodStr = lastmodStr;
+    //     this.timezone = timezone;
+    // },
     data(vm) {
         return {
             lastmodStr: "",
@@ -34,10 +34,12 @@ export default {
         <p>
             <NuxtLink to="/trpg">/trpg</NuxtLink>
         </p>
+
         <div class="footer__inner">
             <p>last modified at: {{ lastmodStr + " " + timezone }}</p>
             <p>© 2022 cheerppy</p>
         </div>
+
     </div>
 </template>
 
