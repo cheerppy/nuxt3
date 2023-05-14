eb5ffadd2151654fdbfe66277135d7dc2a6b99eb
cheerppy <john0217smith@gmail.com>
2023-05-15 00:33:06 +0900

    add CDN URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        baseURL: "/nuxt3/"
+        baseURL: "/nuxt3/",
+        cdnURL: "/nuxt3/",
     }
 
 })
