259c5473ab461430c4eac55f0c015a4dc9a2a2bd
cheerppy <john0217smith@gmail.com>
2023-05-15 00:08:12 +0900

    add pages

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <p>Hello Nuxt</p>
+        <NuxtPage />
     </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div>
+        <p>Hello Nuxt</p>
+    </div>
+</template>
+
+<style scoped></style>
