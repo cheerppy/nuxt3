048492509d642d2afff5c8990efe660508fea5a5
cheerppy <john0217smith@gmail.com>
2023-05-15 03:01:08 +0900

    update baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        baseURL: "/nuxt3/",
-        cdnURL: "/nuxt3/",
+        baseURL: "/trpg-note/",
+        cdnURL: "/trpg-note/",
     }
 
 })
